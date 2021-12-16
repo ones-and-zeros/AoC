727e5794419d9e466d92b846a2dd18c2e77cae1d
Anthony <anthonyarmbruster@gmail.com>
2021-12-16 15:05:01 -0500

    make recursive

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <bits/stdc++.h>
-#include <cctype>
 using namespace std;
 
+map<string,string> lookup;
+
+#define max_key_len ((size_t)1024)
+
+string convert(const string& s)
+{
+    if(s.size() == 1)
+        return s;
+
+    if(s.size() <= max_key_len)
+        if(lookup.find(s) != lookup.end())
+            return s[0] + lookup[s] + s[s.size() - 1];
+
+    const string& str1 {s.substr(0, s.size() >> 1)};
+    const string& str2 {s.substr(str1.size(), s.size() - str1.size())};
+    const string& str_m {s.substr(str1.size() - 1, 2)};
+
+    string out = convert(str1) + lookup[str_m] + convert(str2);
+
+    if(s.size() <= max_key_len)
+        lookup.insert({s, out.substr(1, out.size() - 2)});
+
+    return out;
+}
+
+size_t calc(string s, size_t cnt)
+{
+    for(size_t i = 0; i < cnt; i++)
+        s = convert(s);
+
+    map<char,size_t> char_counts;
+    for(auto c : s)
+        char_counts[c]++;
+
+    vector<size_t> tallies;
+    for(auto cc: char_counts)
+        tallies.push_back(cc.second);
+    sort(tallies.begin(), tallies.end());
+    
+    return tallies[tallies.size()-1] - tallies[0]; //max - min
+}
 
 int main()
 {
@@ -14,8 +54,6 @@ int main()
         string file_line;
 
         string input_s;
-
-        map<string,char> lookup;
 
         //parse lines
         while(getline(infile, file_line))
@@ -34,58 +72,45 @@ int main()
             getline(iss, key, ' ');
             getline(iss, junk, ' ');
             getline(iss, val, ' ');
-            lookup.insert({key, val[0]});
+            lookup.insert({key, val});
         }
-
 
         for(auto lu : lookup)
             cout << lu.first << " : " << lu.second << endl;
         cout << endl;
         cout << "input : " << input_s << endl;
-
-        string out_s = input_s;
-        for(size_t i = 0; i < 10; i++)
-        {
-            string to_add;
-            for(size_t j = 0; j < out_s.size() - 1; j+=1)
-                to_add += lookup[out_s.substr(j,2)];
-
-            for(size_t j = 0; j < to_add.size(); j++)
-                out_s.insert( 1+(j*2), 1, to_add[j]);
-
-//            cout << "step " << i+1 << ": " << out_s << endl;
-        }
         cout << endl;
 
-        map<char,size_t> char_counts;
-        for(auto c : out_s)
-            char_counts[c]++;
 
-        cout << "char counts:" << endl;
-        for(auto cc: char_counts)
-            cout << cc.first << " : " << cc.second << endl;
+        start = chrono::high_resolution_clock::now();
+        size_t answer = calc(input_s, 10);
+        auto stop = chrono::high_resolution_clock::now();
+        cout << "a - answer 10: " << answer << endl;
+        auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+        cout << "sec: " << (duration.count() / 1000000) << 
+                        "." <<  setfill('0') << setw(6) << (duration.count() % 1000000) << endl;
         cout << endl;
 
-        vector<size_t> tallies;
-        for(auto cc: char_counts)
-            tallies.push_back(cc.second);
-        sort(tallies.begin(), tallies.end());
-        
-        size_t min = tallies[0];
-        size_t max = tallies[tallies.size()-1];
-        cout << "min: " << min << "  max: " << max << endl;
 
-        size_t answer = max - min;
-        cout << "answer: " << answer << endl;
+        start = chrono::high_resolution_clock::now();
+        answer = calc(input_s, 20);
+        stop = chrono::high_resolution_clock::now();
+        cout << "    answer 20: " << answer << endl;
+        duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+        cout << "sec: " << (duration.count() / 1000000) << 
+                        "." <<  setfill('0') << setw(6) << (duration.count() % 1000000) << endl;
+        cout << endl;
+
+
         cout << endl;
 
         infile.close();
     }
 
     // execution time
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    cout << "sec: " << (duration.count() / 1000000) << 
-                    "." <<  setfill('0') << setw(6) << (duration.count() % 1000000) << endl;
-    cout << endl;
+    // auto stop = chrono::high_resolution_clock::now();
+    // auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    // cout << "sec: " << (duration.count() / 1000000) << 
+    //                 "." <<  setfill('0') << setw(6) << (duration.count() % 1000000) << endl;
+    // cout << endl;
 }
