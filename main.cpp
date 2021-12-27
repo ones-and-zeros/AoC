c206ae8d03b1490a70aa4fc627322701c1a206e6
Anthony <anthonyarmbruster@gmail.com>
2021-12-26 23:53:09 -0500

    solution a

--- main.cpp
+++ main.cpp
@@ -11,129 +11,64 @@ Image image_org;
 constexpr char p_on =  '#';
 constexpr char p_off = '.';
 
-constexpr size_t image_axis_exp = 2;
+char background = p_off;
 
+constexpr size_t image_axis_exp = 2; //expands 1 unit in each direction
 
 Image image_convert(const Image& in)
 {
+    size_t x_out_qty = in.size() + image_axis_exp;
+    size_t y_out_qty = in[0].size() + image_axis_exp;
+
+    //create a modified input, padded so that the following calcs do not overrun
+    Image in_mod;
+    in_mod.push_back(string(y_out_qty + image_axis_exp, background));
+    in_mod.push_back(string(y_out_qty + image_axis_exp, background));
+    for(auto r : in)
+    {
+        string row;
+        row += background;
+        row += background;
+        row += r;
+        row += background;
+        row += background;
+        in_mod.push_back( row );
+    }
+    in_mod.push_back(string(y_out_qty + image_axis_exp, background));
+    in_mod.push_back(string(y_out_qty + image_axis_exp, background));
+
     Image out;
-    int32_t x_out_qty = in.size() + image_axis_exp;
-    int32_t y_out_qty = in[0].size() + image_axis_exp;
-
-    int32_t x_in_min = 0;
-    int32_t x_in_max = in.size() - 1;
-
-    int32_t y_in_min = 0;
-    int32_t y_in_max = in[0].size() - 1;
-
-    for(int32_t x = 0; x < x_out_qty; x++)
+    for(size_t x = 0; x < x_out_qty; x++)
     {
         out.push_back("");
-        for(int32_t y = 0; y < y_out_qty; y++)
+        for(size_t y = 0; y < y_out_qty; y++)
         {
             size_t decode_offset = 0; //9 bitwise
 
-            int32_t x_input = x - 1;
-            int32_t y_input = y - 1;
+            size_t x_input = x + 1;
+            size_t y_input = y + 1;
 
             // bit 8
-            if( x_input >= x_in_min + 1 &&
-                x_input <= x_in_max + 1 &&
-                y_input >= y_in_min + 1 &&
-                y_input <= y_in_max + 1 &&
-                p_on == in[x_input - 1][y_input - 1] )
-            {
-                decode_offset |= 1UL << 8;
-            }
-
-            // bit 7
-            if( x_input >= x_in_min + 1 &&
-                x_input <= x_in_max + 1 &&
-                y_input >= y_in_min + 0 &&
-                y_input <= y_in_max + 0 &&
-                p_on == in[x_input - 1][y_input] )
-            {
-                decode_offset |= 1UL << 7;
-            }
-
-            // bit 6
-            if( x_input >= x_in_min + 1 &&
-                x_input <= x_in_max + 1 &&
-                y_input >= y_in_min - 1 &&
-                y_input <= y_in_max - 1 &&
-                p_on == in[x_input - 1][y_input + 1] )
-            {
-                decode_offset |= 1UL << 6;
-            }
-
-            // bit 5
-            if( x_input >= x_in_min + 0 &&
-                x_input <= x_in_max + 0 &&
-                y_input >= y_in_min + 1 &&
-                y_input <= y_in_max + 1 &&
-                p_on == in[x_input][y_input - 1] )
-            {
-                decode_offset |= 1UL << 5;
-            }
-
-            // bit 4
-            if( x_input >= x_in_min + 0 &&
-                x_input <= x_in_max + 0 &&
-                y_input >= y_in_min + 0 &&
-                y_input <= y_in_max + 0 &&
-                p_on == in[x_input][y_input] )
-            {
-                decode_offset |= 1UL << 4;
-            }
-
-            // bit 3
-            if( x_input >= x_in_min + 0 &&
-                x_input <= x_in_max + 0 &&
-                y_input >= y_in_min - 1 &&
-                y_input <= y_in_max - 1 &&
-                p_on == in[x_input][y_input + 1] )
-            {
-                decode_offset |= 1UL << 3;
-            }
-
-            // bit 2
-            if( x_input >= x_in_min - 1 &&
-                x_input <= x_in_max - 1 &&
-                y_input >= y_in_min + 1 &&
-                y_input <= y_in_max + 1 &&
-                p_on == in[x_input + 1][y_input - 1] )
-            {
-                decode_offset |= 1UL << 2;
-            }
-
-            // bit 1
-            if( x_input >= x_in_min - 1 &&
-                x_input <= x_in_max - 1 &&
-                y_input >= y_in_min + 0 &&
-                y_input <= y_in_max + 0 &&
-                p_on == in[x_input + 1][y_input] )
-            {
-                decode_offset |= 1UL << 1;
-            }
-
-            // bit 0
-            if( x_input >= x_in_min - 1 &&
-                x_input <= x_in_max - 1 &&
-                y_input >= y_in_min - 1 &&
-                y_input <= y_in_max - 1 &&
-                p_on == in[x_input + 1][y_input + 1] )
-            {
-                decode_offset |= 1UL << 0;
-            }
+            if(p_on == in_mod[x_input - 1][y_input - 1])  decode_offset |= 1UL << 8;
+            if(p_on == in_mod[x_input - 1][y_input + 0])  decode_offset |= 1UL << 7;
+            if(p_on == in_mod[x_input - 1][y_input + 1])  decode_offset |= 1UL << 6;
+            if(p_on == in_mod[x_input + 0][y_input - 1])  decode_offset |= 1UL << 5;
+            if(p_on == in_mod[x_input + 0][y_input + 0])  decode_offset |= 1UL << 4;
+            if(p_on == in_mod[x_input + 0][y_input + 1])  decode_offset |= 1UL << 3;
+            if(p_on == in_mod[x_input + 1][y_input - 1])  decode_offset |= 1UL << 2;
+            if(p_on == in_mod[x_input + 1][y_input + 0])  decode_offset |= 1UL << 1;
+            if(p_on == in_mod[x_input + 1][y_input + 1])  decode_offset |= 1UL << 0;
 
             out[x] += pixel_decoder[decode_offset];
         }
     }
 
+    background = (background == p_on ? pixel_decoder[511] : pixel_decoder[0]);
+
     return out;
 }
 
-ostream& operator<<(ostream& os, Image image)
+ostream& operator<<(ostream& os, const Image& image)
 {
     for(auto row : image)
         os << row << '\n';
@@ -155,17 +90,11 @@ int main()
         while(getline(infile, line))
         {
             if(line.empty())
-            {
                 parse_decoder = false;
-                continue;
-            }
-            if(parse_decoder)
-            {
+            else if(parse_decoder)
                 pixel_decoder += line;
-                continue;
-            }
-
-            image_org.push_back(line);
+            else
+                image_org.push_back(line);
         }
 
         cout << "decoder:\n";
@@ -174,25 +103,29 @@ int main()
         cout << '\n';
         cout << "original:\n";
         cout << image_org;
+        cout << image_org.size() << " x " << image_org[0].size() << '\n';
         cout << '\n';
 
-
         vector<string> image_mod = image_org;
+        background = p_off;
         for(size_t i = 0; i < 2; i++)
         {
             image_mod = image_convert(image_mod);
 
             cout << "convert out" << i+1 << ":\n";
             cout << image_mod;
+            cout << image_mod.size() << " x " << image_mod[0].size() << '\n';
             cout << '\n';
         }
-        size_t count = 0;
+        unsigned long long count_on = 0;
+        unsigned long long count_off = 0;
         for( auto row : image_mod )
             for(auto c : row)
                 if(c == p_on)
-                    count++;
-        cout << " a - pixel count after 2 passes: " << count << '\n';
-
+                    count_on++;
+                else if(c == p_off)
+                    count_off++;
+        cout << " a - light pixel count after 2 passes: " << count_on << "\n";
 
         infile.close();
     }
