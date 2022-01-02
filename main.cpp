bcdaee23bf19c086ae164fd40b80c52b8b937591
Anthony <anthonyarmbruster@gmail.com>
2022-01-02 14:19:21 -0500

    solution b

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using Point = pair<int,int>;
+
+void update_point(Point& point, char move)
+{
+    switch(move)
+    {
+        case 'v':
+            point.first--;
+            break;            
+        case '^':
+            point.first++;
+            break;
+        case '<':
+            point.second--;
+            break;
+        case '>':
+            point.second++;
+            break;
+    }
+}
 
 int main()
 {
@@ -21,33 +41,32 @@ int main()
                 moves = line;
         }
 
-        using Point = pair<int,int>;
         map<Point, uint64_t> houses;
-        
-        int x = 0;
-        int y = 0;
-        houses[{x,y}]++; //starting house gets a present
+
+        Point santa = {0,0};
         for(auto m : moves)
         {
-            switch(m)
-            {
-                case '<':
-                    y--;
-                    break;
-                case '>':
-                    y++;
-                    break;
-                case '^':
-                    x++;
-                    break;
-                case 'v':
-                    x--;
-                    break;            
-            }
-            houses[{x,y}]++;
+            update_point(santa, m);
+            houses[santa]++;
         }
-
         cout << " a - qty of houses: " << houses.size() << "\n"; 
+
+
+
+        constexpr size_t real = 0;
+        constexpr size_t robo = 1;
+        vector<Point> santas{ {0,0}, {0,0}};
+        houses.clear();
+        houses[santas[real]]++; //starting house gets a present
+        houses[santas[robo]]++; //starting house gets a present
+        size_t s = real;
+        for(auto m : moves)
+        {
+            update_point(santas[s], m);
+            houses[santas[s]]++;
+            s = (real == s) ? robo : real;
+        }
+        cout << " b - qty of houses: " << houses.size() << "\n"; 
 
         cout << '\n';
         infile.close();
