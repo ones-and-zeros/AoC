3157147b0aa29d3d4b97d6009d02d6fa2f2863b8
Anthony <anthonyarmbruster@gmail.com>
2022-01-11 16:25:00 -0500

    solution a

--- 2015/day25/main.cpp
+++ 2015/day25/main.cpp
@@ -1,20 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-uint32_t find_code(uint32_t row, uint32_t col)
+uint64_t find_code(uint32_t row, uint32_t col)
 {
-    uint32_t coded_val = 20151125;
-
-    // ex.
-    coded_val = 1;
-
+    uint64_t coded_val = 20151125;
     uint32_t depth = 2;
 
     while(1)
     {
         for(uint32_t x = 1; x <= depth; x++)
         {
-            coded_val++;
+            coded_val = (coded_val * 252533) % 33554393;
 
             if( col == x &&
                 row == (1 + depth - x))
@@ -54,12 +50,14 @@ int main()
 
     // ex...
     auto code = find_code(4, 2);
-    if(code != 12)
+    if(code != 32451966)
         throw logic_error("example failed");
     code = find_code(1, 5);
-    if(code != 15)
+    if(code != 10071777)
         throw logic_error("example failed");
 
+    code = find_code(3010, 3019);
+    cout << "a) code {3010,3019}: " << code << "\n";
 
     // execution time
     auto stop = chrono::high_resolution_clock::now();
