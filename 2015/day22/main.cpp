0ee376d7db47561f1474cf39b7bd2b50060b8ffa
Anthony <anthonyarmbruster@gmail.com>
2022-01-11 02:03:25 -0500

    refactor and purge unused code

--- 2015/day22/main.cpp
+++ 2015/day22/main.cpp
@@ -1,10 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// a) 1415 too high
-// a) 1362 too high
-// a) 649 too low
-
 struct Spell{
     string name;
     int mana_cost;
@@ -87,25 +83,13 @@ vector<Spell> best_spell_log;
 void spell_effect(Spell spell)
 {
     if(spell.damage)
-    {
-//        cout << "  boss hp -" << spell.damage; 
         game_boss.hp -= spell.damage;
-    }
     if(spell.armor)
-    {
-//        cout << "  arm +" << spell.armor; 
         game_player.armor += spell.armor;
-    }
     if(spell.hp_heal)
-    {
-//        cout << "  hp +" << spell.hp_heal; 
         game_player.hp += spell.hp_heal;
-    }
     if(spell.mana_heal)
-    {
-//        cout << "  mana +" << spell.mana_heal; 
         game_player.mana += spell.mana_heal;
-    }    
 }
 
 void spells_effect()
@@ -115,99 +99,18 @@ void spells_effect()
     while(pos < game_active_spells.size())
     {
         auto& spell = game_active_spells[pos];
-//        cout << spell.name << ":";
 
         spell_effect(game_active_spells[pos]);
 
         --spell.duration;
-//        cout << "  timer " << spell.duration << "\n";
         if(spell.duration == 0)
         {
-//            cout << spell.name << " expires\n";
             game_active_spells.erase(game_active_spells.begin() + pos);
             continue; // don't increment
         }
         pos++;
     }
 }
-
-bool play_round(Spell spell)
-{
-    /* player turn */
-//    cout << "\n-- Player turn --\n";
-//    print_score(game_player, game_boss);
-    spells_effect();
-    if(game_boss.hp <= 0)
-        return false;
-    // Player casts spell
-    game_player.mana -= spell.mana_cost;
-    if(game_player.mana < 0)
-        return false;
-//    cout << "Player casts " << spell.name;
-    if(spell.duration == 1)
-    {
-//        cout << ":";
-        spell_effect(spell);
-//        cout << "\n";
-    }
-    else
-    {
-//        cout << "\n";
-        game_active_spells.push_back(spell);
-    }
-
-    /* boss turn */
-//    cout << "\n-- Boss turn --\n";
-//    print_score(game_player, game_boss);
-    spells_effect();
-    if(game_boss.hp <= 0)
-        return true;
-    // boss attacks
-    int damage_to_player = (game_boss.damage - game_player.armor);
-    if(damage_to_player < 1)
-        damage_to_player = 1;
-//    cout << "Boss attacks, damage is " << damage_to_player << "\n";
-    game_player.hp -= damage_to_player;
-    if(game_player.hp <= 0)
-        return true;
-
-    return true;
-}
-
-pair<bool, Player> play_game(Player player, Player boss, vector<Spell> spellorder)
-{
-    game_active_spells.clear();
-    game_player = player;
-    game_boss = boss;
-
-//    cout << "\n\n ####### New Game ########\n";
-
-    for(auto cast : spellorder)
-    {
-        if(!play_round(cast))
-            continue;
-
-        if(game_boss.hp <= 0)
-        {
-//            cout << "\nPlayer wins!\n";
-            return {true, game_player};
-        }
-        if(game_player.hp <= 0)
-        {
-//            cout << "\nPlayer loses\n";
-            return {false, game_boss};
-        }
-        if(game_player.mana < 0)
-        {
-//            cout << "Player loses, out of mana\n";
-            return {false, game_boss}; //todo better indication ran out of mana
-        }
-    }
-
-    cout << flush;
-    throw domain_error("ran out of spells");
-}
-
 
 void game_win()
 {
@@ -345,41 +248,25 @@ int main()
         // example 1 - 1
         Player player_ex(10, 0, 0, 250);
         Player boss_ex(13, 8, 0, 0);
-        vector<Spell> spell_que({sp_poison, sp_m_missle});
-        if(pair<bool,Player>({true, Player{2,0,0,24}}) != play_game(player_ex, boss_ex, spell_que))
+        auto result = lowest_mana_win(player_ex, boss_ex, false);
+        if(result.second != vector<Spell>{sp_poison, sp_m_missle})
         {
             cout << flush;
             throw domain_error("example fails");
         }
-//        auto result = lowest_mana_win(player_ex, boss_ex);
 
         // example 1 - 2
         player_ex = {10, 0, 0, 250};
         boss_ex = {14, 8, 0, 0};
-        spell_que = vector<Spell>{sp_recharge, sp_shield, sp_drain, sp_poison, sp_m_missle};
-        if(pair<bool,Player>({true, Player{1,0,0,114}}) != play_game(player_ex, boss_ex, spell_que))
+        result = lowest_mana_win(player_ex, boss_ex, false);
+        if(result.second != vector<Spell>{sp_recharge, sp_shield, sp_drain, sp_poison, sp_m_missle})
         {
             cout << flush;
             throw domain_error("example fails");
         }
-//        auto result = lowest_mana_win(player_ex, boss_ex);
 
-        auto result = lowest_mana_win({50,0,0,500}, {58,9,0,0}, false);
+        result = lowest_mana_win({50,0,0,500}, {58,9,0,0}, false);
         cout << "a) lowest mana win: " << result.first << "\n";
-        // for(auto s : result.second)
-        // {
-        //     cout << "  " << s.name << ",\t" << s.mana_cost << " mana\n";
-        // }
-        // cout << "game end count: " << game_end_count << "\n";
-
-        // uint64_t total = 0;
-        // for(auto s : result.second)
-        // {
-        //     cout << "  " << s.name << ",\t" << s.mana_cost << " mana\n";
-        //     total += s.mana_cost;
-        // }
-        // cout << "win " << game_win_count << " out of " << game_end_count << "\n";
-
 
         result = lowest_mana_win({50,0,0,500}, {58,9,0,0}, true);
         cout << "b) lowest mana win hard: " << result.first << "\n";
