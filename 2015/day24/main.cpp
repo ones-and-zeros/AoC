7fe49376745e4fd92e8de7efe4f60196ca62b8ff
Anthony <anthonyarmbruster@gmail.com>
2022-01-11 11:44:33 -0500

    day24 template

---
+++ 2015/day24/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    auto start = chrono::high_resolution_clock::now();
+
+    // execute code
+    ifstream infile("input.txt");
+    if(infile.is_open())
+    {
+        //parse lines
+        string line;
+        while(getline(infile, line))
+        {
+//            istringstream iss{line};
+        }
+
+
+
+
+
+
+        cout << '\n';
+        infile.close();
+    }
+
+    // execution time
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << "sec: " << (duration.count() / 1000000) << 
+                    "." <<  setfill('0') << setw(6) << (duration.count() % 1000000) << endl;
+    cout << endl;
+}
