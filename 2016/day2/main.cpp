428e5d1f7dd07f5c896356bd1ec2c0b9a092f273
Anthony <anthonyarmbruster@gmail.com>
2022-01-28 13:53:00 -0500

    refactor Keypad class

--- 2016/day2/main.cpp
+++ 2016/day2/main.cpp
@@ -3,18 +3,9 @@ using namespace std;
 
 #include "timer.h"
 
-vector<string> keypad{
-    {"123"},
-    {"456"},
-    {"789"}
-};
-
-constexpr int32_t keypad_axis_len{3};
-constexpr int32_t keypad_step{1};
-
-class Keypad_place{
+class Keypad{
 public:
-    Keypad_place()
+    Keypad()
     {
     }
     char update_pos(string commands)
@@ -22,38 +13,44 @@ public:
         for(auto c : commands)
         {
             int32_t &axis = ((c == 'U') || (c =='D')) ? x : y;
+            axis += ((c == 'D') || (c =='R')) ? keypad_step : -keypad_step;
 
-            if((c == 'D') || (c =='R'))
+            if(axis < 0)
             {
-                if(axis + keypad_step < keypad_axis_len)
-                {
-                    axis += keypad_step;
-                }
+                axis = 0;
             }
-            else
+            else if(axis >= keypad_axis_len)
             {
-                if(axis - keypad_step >= 0)
-                {
-                    axis -= keypad_step;
-                }
+                axis = keypad_axis_len - 1;
             }
         }
-        return keypad[x][y];
+
+        return keys[x][y];
     }
 private:
-    int32_t x = 1;
-    int32_t y = 1;
+    int32_t x = 1; //point to '5'
+    int32_t y = 1; //point to '5'
+    // consts
+    static constexpr int32_t keypad_axis_len{3};
+    static constexpr int32_t keypad_step{1};
+    static constexpr array<array<char,keypad_axis_len>, keypad_axis_len> keys{
+        array<char,keypad_axis_len>{'1','2','3'},
+        array<char,keypad_axis_len>{'4','5','6'},
+        array<char,keypad_axis_len>{'7','8','9'} };
 };
+
+// needs this declaration outside of class to avoid link error when accessing within class
+constexpr array<array<char,Keypad::keypad_axis_len>, Keypad::keypad_axis_len> Keypad::keys;
 
 string find_code(vector<string> commands)
 {
     Timer t("find-code");
-    Keypad_place place;
+    Keypad keypad;
     string code;
 
     for(auto command : commands)
     {
-        code += place.update_pos(command);
+        code += keypad.update_pos(command);
     }
 
     return code;
