5afd39c92015f71f66fe1286e7ec6b3ed2371703
Anthony <anthonyarmbruster@gmail.com>
2022-01-29 11:22:25 -0500

    refactor part 1

--- 2016/day4/main.cpp
+++ 2016/day4/main.cpp
@@ -9,6 +9,59 @@ public:
     : name_{name}, sector_id_{sector_id}, checksum_{checksum}
     {
     }
+    bool is_valid() const
+    {
+        return checksum_ == gen_checksome();
+    }
+    int32_t sector_id_val() const
+    {
+        return stol(sector_id_);
+    }
+private:
+    struct Letter_count{
+        Letter_count(char letter, int32_t count)
+        : letter_{letter}, count_{count}
+        {
+        }
+        char letter_;
+        int32_t count_;
+
+        // for priority_queue using 'std::less'
+        bool operator<(const Letter_count &other)
+        {
+            if(this->count_ != other.count_)
+                return (this->count_ > other.count_);
+            return (this->letter_ < other.letter_);
+        }
+    };
+
+    string gen_checksome() const
+    {
+        // count letters
+        array<int32_t,26> letters{0};
+        for(auto c : name_)
+        {
+            if(isalpha(c))
+            {
+                letters[c - 'a']++;
+            }
+        }
+
+        vector<Letter_count> letter_count;
+        for(char letter = 'a'; letter <= 'z'; letter++)
+        {
+            letter_count.push_back({letter, letters[letter - 'a']});
+        }
+        partial_sort(letter_count.begin(), letter_count.begin() + 5, letter_count.end());
+        
+        string result;
+        for(size_t pos = 0; pos < 5; pos++)
+        {
+            result += letter_count[pos].letter_;
+        }
+
+        return result;
+    }
     string name_;
     string sector_id_;
     string checksum_;
@@ -16,50 +69,6 @@ public:
 
 using Rooms = vector<Room>;
 
-struct Letter_count{
-    Letter_count(char letter, int32_t count)
-    : letter_{letter}, count_{count}
-    {
-    }
-    char letter_;
-    int32_t count_;
-};
-
-// for priority_queue using 'std::less'
-bool operator<(Letter_count a, Letter_count b)
-{
-    if(a.count_ != b.count_)
-        return (a.count_ > b.count_);
-    return (a.letter_ < b.letter_);
-}
-
-string checksum(string name)
-{
-    // count letters
-    array<int32_t,26> letters{0};
-    for(auto c : name)
-    {
-        if(isalpha(c))
-        {
-            letters[c - 'a']++;
-        }
-    }
-
-    vector<Letter_count> letter_count;
-    for(char letter = 'a'; letter <= 'z'; letter++)
-    {
-        letter_count.push_back({letter, letters[letter - 'a']});
-    }
-    partial_sort(letter_count.begin(), letter_count.begin() + 5, letter_count.end());
-    
-    string result;
-    for(size_t pos = 0; pos < 5; pos++)
-    {
-        result += letter_count[pos].letter_;
-    }
-
-    return result;
-}
 
 int32_t valid_room_sum(Rooms rooms)
 {
@@ -68,9 +77,9 @@ int32_t valid_room_sum(Rooms rooms)
 
     for(auto r : rooms)
     {
-        if(r.checksum_ == checksum(r.name_))
+        if(r.is_valid())
         {
-            tally += stol(r.sector_id_);
+            tally += r.sector_id_val();
         }
     }
     return tally;
