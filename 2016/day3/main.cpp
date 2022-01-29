68a789fd9191182c2e84e8838b6936af5aa5cd3e
Anthony <anthonyarmbruster@gmail.com>
2022-01-28 22:04:31 -0500

    2016 day3 template

---
+++ 2016/day3/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "timer.h"
+
+int main()
+{
+    Timer t_main("main");
+
+    ifstream infile("input.txt");
+    if(infile.is_open())
+    {
+        Timer t_p("parse input");
+
+        // string line;
+        // while(getline(infile, line))
+        // {
+        //     commands.push_back(line);
+        // }
+        // infile.close();
+    }
+    else
+        throw logic_error("unable to open input file");
+
+
+}
