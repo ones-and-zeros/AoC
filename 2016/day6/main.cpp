baa7002db1605050ca073e5f29b56b360f777e0f
Anthony <anthonyarmbruster@gmail.com>
2022-01-30 08:19:38 -0500

    day 6 part1 solution

--- 2016/day6/main.cpp
+++ 2016/day6/main.cpp
@@ -3,9 +3,45 @@ using namespace std;
 
 #include "timer.h"
 
+using Messages = vector<string>;
+
+string recover(Messages messages)
+{
+    Timer t_main("recover");
+    
+    if(messages.empty())
+        return "(no messages)";
+
+    string result;
+
+    for(size_t pos = 0; pos < messages[0].size(); ++pos)
+    {
+        array<uint32_t, 26> letter_count{0};
+        for(auto & m : messages)
+            ++letter_count[m[pos] - 'a'];
+
+        uint32_t max_val = 0;
+        char letter = '?';
+        for(size_t lpos = 0; lpos < 26; ++lpos)
+        {
+            if(letter_count[lpos] > max_val)
+            {
+                max_val = letter_count[lpos];
+                letter = 'a' + lpos;
+            }
+        }
+        result += letter;
+    }
+
+    return result;
+}
+
+
 int main()
 {
     Timer t_main("main");
+
+    Messages messages;
 
     ifstream infile("input.txt");
     if(infile.is_open())
@@ -15,7 +51,7 @@ int main()
         string line;
         while(getline(infile, line))
         {
-//            input = line;
+            messages.push_back(line);
         }
 
         infile.close();
@@ -24,5 +60,7 @@ int main()
         throw logic_error("unable to open input file");
     cout << "\n";
 
-
+    auto result = recover(messages);
+    cout << "part 1 - recovered: " << result << "\n";
+    cout << "\n";
 }
