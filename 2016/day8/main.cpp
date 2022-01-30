d5ef5b76412ac20a4992aca2a8d4e5dd39d1816b
Anthony <anthonyarmbruster@gmail.com>
2022-01-30 11:28:35 -0500

    2016 day8 template

---
+++ 2016/day8/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "timer.h"
+
+int main()
+{
+    Timer t_main("main");
+
+    ifstream infile("input.txt");
+    if(infile.is_open())
+    {
+        Timer t_p("parse input");
+
+        string line;
+        while(getline(infile, line))
+        {
+            // todo
+        }
+
+        infile.close();
+    }
+    else
+        throw logic_error("unable to open input file");
+    cout << "\n";
+}
