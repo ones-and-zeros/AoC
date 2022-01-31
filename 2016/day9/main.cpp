7d4e7d9775e597108bb6c61b082f70e21eb7a11d
Anthony <anthonyarmbruster@gmail.com>
2022-01-30 21:26:40 -0500

    day 9 solutions

--- 2016/day9/main.cpp
+++ 2016/day9/main.cpp
@@ -3,19 +3,67 @@ using namespace std;
 
 #include "timer.h"
 
+uint64_t decomp_help(const string & in, bool do_full = false)
+{
+    uint64_t length = 0;
+    istringstream iss{in};
+    char c;
+    while(iss >> c)
+    {
+        if(c == ')')
+            throw logic_error("invaid terminator");
+
+        if(c != '(')
+        {
+            length++;
+            continue;
+        }
+
+        // '(' found so extract
+        int32_t size, qty;
+        iss >> size;
+        iss >> c; // junk 'x'
+        iss >> qty;
+        iss >> c; // junk ')'
+
+        string substring;
+        for(size_t n = 0; n < size; n++)
+        {
+            iss >> c;
+            substring += c;
+        }
+
+        if(do_full)
+            length += decomp_help(substring, true) * qty;
+        else
+            length += size * qty;
+    }
+
+    return length;
+}
+
+uint64_t decompressed_length(const string & in, bool do_full = false)
+{
+    Timer t{"decompressed_length"};
+    return decomp_help(in, do_full);
+}
+
 int main()
 {
     Timer t_main("main");
+
+    string input;
 
     ifstream infile("input.txt");
     if(infile.is_open())
     {
         Timer t_p("parse input");
 
+
         string line;
         while(getline(infile, line))
         {
-            // todo
+            input = line;
         }
 
         infile.close();
@@ -23,4 +71,12 @@ int main()
     else
         throw logic_error("unable to open input file");
     cout << "\n";
+
+    uint64_t result = decompressed_length(input);
+    cout << "part 1 - decompressed length: " << result << "\n";
+    cout << endl;
+
+    result = decompressed_length(input, true);
+    cout << "part 2 - decompressed length full: " << result << "\n";
+    cout << endl;
 }
