ba3b8c28a4a7b390420f200d8acf12edef6319fd
Anthony <anthonyarmbruster@gmail.com>
2022-12-18 11:46:50 -0500

    Solved day 1 part 1

--- 2022/day01/main.cpp
+++ 2022/day01/main.cpp
@@ -1,27 +1,84 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <cstdint>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <numeric>
 
 #include "timer.h"
 
+using Calories = int;
+using FoodItems = std::vector<Calories>;
+using ElvesInventory = std::vector<FoodItems>;
+
 int main()
 {
-    Timer t_main("main");
+  Timer t_main("main");
 
-    ifstream infile("input.txt");
-    if(infile.is_open())
-    {
-        Timer t_p("parse input");
+  ElvesInventory elves_inventory;
 
-        string line;
-        while(getline(infile, line))
-        {
-            // todo
-        }
+  std::ifstream infile{"input.txt"};
+  if (infile.is_open()) {
+    Timer t_p("parse input");
 
-        infile.close();
+    FoodItems food_items;
+
+    std::string line;
+    while (getline(infile, line)) {
+      if (line.empty()) {
+        elves_inventory.push_back(food_items);
+        food_items.clear();
+        continue;
+      }
+
+      std::istringstream iss{line};
+      std::string calories_s;
+      iss >> calories_s;
+
+      // calories
+      food_items.push_back(std::stoi(calories_s));
+
+      // todo
     }
-    else
-        throw logic_error("unable to open input file");
-    cout << "\n";
+
+    infile.close();
+  }
+  else
+    throw std::logic_error("unable to open input file");
+  std::cout << "\n";
+
+  // for (std::size_t idx = 0; idx < elves_inventory.size(); idx++) {
+  //   const auto& inventory = elves_inventory[idx];
+  //   auto total_calories = std::accumulate(inventory.begin(), inventory.end(), 0); 
+  //   std::cout << "Elf " << (idx + 1) << " = " <<
+  //      total_calories << ":\n";
+  //   for (const auto food_item : inventory) {
+  //     std::cout << "  " << food_item << "\n";   
+  //   }
+  // }
+ 
+  struct Elf {
+    std::size_t id;
+    int total_calories;
+  };
+
+  Elf elf_with_max_calories{0,0};
+
+  {
+    Timer t_main("calc");
+ 
+    for (std::size_t idx = 0; idx < elves_inventory.size(); idx++) {
+      const auto& inventory = elves_inventory[idx];
+      auto total_calories = std::accumulate(inventory.begin(), inventory.end(), 0); 
+      if(total_calories > elf_with_max_calories.total_calories) {
+        elf_with_max_calories.id = idx + 1;
+        elf_with_max_calories.total_calories = total_calories;
+      }
+    }
+  }
+
+  std::cout << "Elf most calories: Elf " << elf_with_max_calories.id <<
+               ", with " << elf_with_max_calories.total_calories << " total calories.\n";
+
 
 }
