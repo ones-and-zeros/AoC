fa74e45848a6b35083f9c198c3b0ef2ca2f64c44
Anthony <anthonyarmbruster@gmail.com>
2022-12-18 19:42:10 -0500

    day 3, part 2 solved

--- 2022/day03/main.cpp
+++ 2022/day03/main.cpp
@@ -1,13 +1,61 @@
+#include <algorithm>
+#include <array>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "timer.h"
 
+using RutsackContents = std::string;
+using RutsackList = std::vector<RutsackContents>;
+
+char RutsackParse(const RutsackContents& contents) {
+  const std::size_t compartment_size = contents.size() / 2;
+  const auto b_begin = contents.begin() + compartment_size;
+  const std::string& compartment_a{contents.begin(), b_begin};
+  const std::string& compartment_b{b_begin, contents.end() };
+
+  for(const auto& item : compartment_a) {
+    const auto result = compartment_b.find(item);
+    if(result != std::string::npos) {
+      return compartment_b[result];
+    }
+  }
+
+  return '0';
+}
+
+char BunchParse(std::array<const RutsackContents*, 3> bunch) {
+  const RutsackContents& elf_a = *bunch[0];
+  const RutsackContents& elf_b = *bunch[1];
+  const RutsackContents& elf_c = *bunch[2];
+
+  for (const auto& item : elf_a) {
+    const auto result_b = elf_b.find(item);
+    if(result_b == std::string::npos) continue;
+
+    const auto result_c = elf_c.find(item);
+    if(result_c == std::string::npos) continue;
+
+    return item;
+  }
+
+  return '0';
+}
+
+constexpr std::uint32_t ItemValue(const char c) {
+  if (!std::isalpha(c)) return 0;
+  if (std::islower(c)) return c - 'a' + 1;
+  return c - 'A' + 27;
+}
 
 int main()
 {
   Timer t_main("main");
+
+  RutsackList rutsack_list;
 
   std::ifstream infile{"input.txt"};
   if (infile.is_open()) {
@@ -15,7 +63,7 @@ int main()
 
     std::string line;
     while (getline(infile, line)) {
-//      strategy_guide.push_back(GetMove(std::istringstream{line}));
+      rutsack_list.push_back(line);
     }
 
     infile.close();
@@ -25,24 +73,41 @@ int main()
   std::cout << "\n";
 
   // // input parse check
-  // for (const auto& move : strategy_guide) {
-  //   std::cout << move.first << " " << move.second << "\n";
+  // for (const auto& line : packing_list) {
+  //   std::cout << line << "\n";
   // }
 
-  // std::uint64_t result_p1 = 0;
-  // std::uint64_t result_p2 = 0;
+  std::uint64_t sum_1 = 0;
+  std::uint64_t sum_2 = 0;
 
   {
     Timer t_main("calc");
 
-    // for (const auto& move : strategy_guide) {
-    //   result_p1 += PlayMatch(DecypherPart1(move));
-    //   result_p1 += PlayMatch(DecypherPart2(move));
-    // }
+    // part 1
+    for (const auto& rutsack : rutsack_list) {
+      const auto result = RutsackParse(rutsack);
+      const auto value = ItemValue(result);
+      sum_1 += value;
+//      std::cout << result << " (" << value << ")\n";
+    }
+
+    // part 2
+    if(rutsack_list.size() % 3 != 0) {
+      throw std::logic_error("lists must be div 3");
+    }
+    for (std::size_t idx = 0; idx < rutsack_list.size(); idx += 3) {
+      const auto result = BunchParse(
+            { &rutsack_list[idx],
+              &rutsack_list[idx + 1],
+              &rutsack_list[idx + 2] });
+      const auto value = ItemValue(result);
+      sum_2 += value;      
+//      std::cout << result << " (" << value << ")\n";
+    }
   }
 
-  // std::cout << "\n";
-  // std::cout << "part 1) total score: " << result_p1 << "\n";
-  // std::cout << "part 2) total score: " << result_p2 << "\n";
-  // std::cout << "\n";
+  std::cout << "\n";
+  std::cout << "part 1) sum: " << sum_1 << "\n";
+  std::cout << "part 2) sum: " << sum_2 << "\n";
+  std::cout << "\n";
 }
