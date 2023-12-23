e94ff68e0c551614903df74a14d5562cedba727c
Anthony Armbruster <anthonyarmbruster@gmail.com>
2023-12-23 01:47:32 -0500

    solved day 9

--- 2023/day09/main.cpp
+++ 2023/day09/main.cpp
@@ -65,31 +65,12 @@ std::int64_t CalcPart1(const Input &input) {
       patterns.push_back(derived_pattern);
     }
 
-    // for (const auto &pattern : patterns) {
-    //   for (const auto &value : pattern) {
-    //     std::cout << " " << value;
-    //   }
-    //   std::cout << "\n";
-    // }
-    // std::cout << "\n";
-
     for (auto pos = 0; pos < patterns.size() - 1; pos++) {
-      auto &to_modify = patterns[patterns.size() - (pos + 2)];
-      const auto &to_reference = patterns[patterns.size() - (1 + pos)];
-
-      // std::cout << " " << *to_modify.rbegin() << " " <<
-      // *to_reference.rbegin() << "\n";
+      auto &to_modify = *(patterns.rbegin() + pos + 1);
+      auto &to_reference = *(patterns.rbegin() + pos);
 
       to_modify.push_back(*to_modify.rbegin() + *to_reference.rbegin());
     }
-
-    // for (const auto &pattern : patterns) {
-    //   for (const auto &value : pattern) {
-    //     std::cout << " " << value;
-    //   }
-    //   std::cout << "\n";
-    // }
-    // std::cout << "\n";
 
     results.push_back(*patterns[0].rbegin());
   }
@@ -100,10 +81,33 @@ std::int64_t CalcPart1(const Input &input) {
 std::int64_t CalcPart2(const Input &input) {
   Timer t_main("calc p2");
 
-  std::int64_t value{};
-  // TODO
+  std::vector<std::int64_t> results{};
 
-  return value;
+  for (const auto &sequence : input) {
+    std::vector<Sequence> patterns{};
+    patterns.push_back(sequence);
+
+    while (!AreAllZero(patterns[patterns.size() - 1])) {
+      Sequence derived_pattern{};
+      const auto &last_pattern = patterns[patterns.size() - 1];
+      for (auto pos = 1; pos < last_pattern.size(); pos++) {
+        derived_pattern.push_back(last_pattern[pos] - last_pattern[pos - 1]);
+      }
+      patterns.push_back(derived_pattern);
+    }
+
+    for (auto pos = 0; pos < patterns.size() - 1; pos++) {
+      auto &to_modify = *(patterns.rbegin() + pos + 1);
+      auto &to_reference = *(patterns.rbegin() + pos);
+
+      const auto new_value = (*to_modify.begin() - *to_reference.begin());
+      to_modify.insert(to_modify.begin(), new_value);
+    }
+
+    results.push_back(*patterns[0].begin());
+  }
+
+  return std::accumulate(results.begin(), results.end(), 0);
 }
 
 } // namespace
